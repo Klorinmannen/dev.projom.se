dbf526ce87c5a8dfe41125792eac6e1e5eb8636a
Jonatan Fransson <jonte.frasse@gmail.com>
2022-08-05 15:23:29 +0200

    user
    fixes for user namespace

--- src/user/logout.php
+++ src/user/logout.php
@@ -1,4 +1,9 @@
 <?php
+
+declare(strict_types=1);
+
+namespace user;
+
 session_unset();
 session_destroy();
-\util\functions::redirect('/');
+util::redirect('/');
--- src/user/util.php
+++ src/user/util.php
@@ -1,11 +1,16 @@
 <?php
+
+declare(strict_types=1);
+
 namespace user;
 
-class util {
+class util
+{
+    public static function add_new_user(
+        string $username,
+        string $password
+    ): void {
 
-    public static function add_new_user(string $username, string $password) {
-
-        // At this point username and password should be ok.
         if (!\util\validate::string($username))
             throw new \Exception('Missing username', 400);
         if (!\util\validate::string($password))
@@ -18,15 +23,17 @@ class util {
 
         static::create($username, $password, false, true);
     }
-    
-    public static function create(string $username,
-                                  string $password,
-                                  bool $api_user = false,
-                                  bool $auto_activate = false): array {
+
+    public static function create(
+        string $username,
+        string $password,
+        bool $api_user = false,
+        bool $activate = false
+    ): array {
 
         $user['Name'] = $username;
         $user['Password'] = \user\password::hash($password);
-        $user['Active'] = $auto_activate ? -1 : 0;
+        $user['Active'] = $activate ? -1 : 0;
 
         if ($api_user)
             $user['JWTKey'] = \util\jwt::create_key();
@@ -34,24 +41,33 @@ class util {
         return \user\model::add($user);
     }
 
-    public static function set_new_api_key(int $user_id): string {
+    public static function set_new_api_key(int $user_id): string
+    {
         if (!\util\validate::id($user_id))
             throw new \Exception('Missing/malformed user id', 400);
 
         $jwt_key = \util\jwt::create_key();
-        \user\model::update_user($user_id, [ 'JWTKey' => $jwt_key ]);
+        \user\model::update_user($user_id, ['JWTKey' => $jwt_key]);
 
         return $jwt_key;
-    }    
-    
-    public static function set_new_password(int $user_id, string $new_password = ''): array {
+    }
+
+    public static function set_new_password(
+        int $user_id,
+        string $new_password = ''
+    ): array {
         if (!\util\validate::id($user_id))
             throw new \Exception('Missing/malformed user id', 400);
 
         $new_password = \user\password::create($new_password);
-        \user\model::update_user($user_id, [ 'Password' => $new_password ]);
+        \user\model::update_user($user_id, ['Password' => $new_password]);
 
         return \user\model::get_user($user_id);
-    }    
+    }
 
+    public static function redirect(string $url): void
+    {
+        header('Location: '.$url);
+        exit;
+    }
 }
