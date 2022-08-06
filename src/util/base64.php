2d5cd9f6ed4846d0035cc61986d205acd4faf8d9
Jonatan Fransson <jonte.frasse@gmail.com>
2022-08-07 00:49:21 +0200

    \util\ json, jwt, base64 cleanup

--- src/util/base64.php
+++ src/util/base64.php
@@ -1,20 +1,28 @@
 <?php
+
+declare(strict_types=1);
+
 namespace util;
 
-class base64 {    
-    public static function encode_url(string $string) {
-        return str_replace(['+','/','='], ['-','_',''], base64_encode($string));
+class base64
+{
+    public static function encode_url(string $string)
+    {
+        return str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($string));
     }
 
-    public static function decode_url(string $string) {
-        return base64_decode(str_replace(['-','_'], ['+','/'], $string));
-    }   
+    public static function decode_url(string $string)
+    {
+        return base64_decode(str_replace(['-', '_'], ['+', '/'], $string));
+    }
 
-    public static function encode(string $string) {
+    public static function encode(string $string)
+    {
         return base64_encode($string);
     }
 
-    public static function decode(srting $string) {
+    public static function decode(string $string)
+    {
         return base64_decode($string);
-    }    
+    }
 }
--- src/util/jwt.php
+++ src/util/jwt.php
@@ -1,106 +1,177 @@
 <?php
+
+declare(strict_types=1);
+
 namespace util;
 
-class jwt {
+class jwt
+{
     public const EXPIRATION_SECONDS = (15 * 60) + 120;
     public const HASH_ALG = 'SHA256';
-    
-    public static function create(int $user_id, string $jwt_key) {
+    public const KEY_BYTES = 30;
+    public const ISSUER = 'dev.projom.se';
+
+    public static function create(
+        int $user_id,
+        string $jwt_key
+    ) {
         if (!$user_id || !$jwt_key)
             return false;
+
         if (!$base64url_header = static::get_header())
             return false;
-        if (!$base64url_payload = static::get_payload($user_id))
+
+        $issued_at = strtotime('now');
+        if (!$base64url_payload = static::get_payload($issued_at, $user_id))
             return false;
+
         if (!$signature = static::generate_signature(static::HASH_ALG, $base64url_header, $base64url_payload, $jwt_key))
             return false;
+
         if (!$token = static::generate_token($base64url_header, $base64url_payload, $signature))
             return false;
+
         if (!static::validate_token_structure($token))
             return false;
+
         return $token;
     }
 
-    private static function get_header() {
-        $header = [ 'alg' => 'SHA256',
-                    'typ' => 'JWT' ];
+    private static function get_header(): string
+    {
+        $header = [
+            'alg' => static::HASH_ALG,
+            'typ' => 'JWT'
+        ];
+
         return static::encode_base64url($header);
     }
 
-    private static function get_payload(int $user_id) {
-        $issued_at = strtotime('now');
-        $payload = [ 'iss' => 'www.projom.se',
-                     'sub' => $user_id,
-                     'iat' => $issued_at,
-                     'exp' => $issued_at + static::EXPIRATION_SECONDS ];
+    private static function get_payload(
+        int $issued_at,
+        int $user_id
+    ): string {
+
+        $payload = [
+            'iss' => static::ISSUER,
+            'sub' => $user_id,
+            'iat' => $issued_at,
+            'exp' => $issued_at + static::EXPIRATION_SECONDS
+        ];
+
         return static::encode_base64url($payload);
     }
 
-    private static function generate_signature(string $alg, string $base64url_header, string $base64url_payload, string $jwt_key) {
-        $data = $base64url_header.'.'.$base64url_payload;
+    private static function generate_signature(
+        string $alg,
+        string $base64url_header,
+        string $base64url_payload,
+        string $jwt_key
+    ): string {
+        $data = $base64url_header . '.' . $base64url_payload;
         return hash_hmac($alg, $data, $jwt_key);
     }
 
-    private static function generate_token(string $base64url_header, string $base64url_payload, string $signature) {
-        return $base64url_header.'.'.$base64url_payload.'.'.$signature;
+    private static function generate_token(
+        string $base64url_header,
+        string $base64url_payload,
+        string $signature
+    ): string {
+        return $base64url_header . '.' . $base64url_payload . '.' . $signature;
     }
 
-    private static function encode_base64url(array $data) {
+    private static function encode_base64url(array $data): string
+    {
         if (!$data)
-            return false;
+            return '';
+
         if (!$json_string = \util\json::encode($data))
-            return false;
+            return '';
+
         return \util\base64::encode_url($json_string);
     }
 
-    private static function validate_token_structure(string $token) {
+    private static function validate_token_structure(string $token): array
+    {
         $parts = explode('.', $token);
+
         if (count($parts) != 3)
-            return false;
+            return [];
+
         if (!$parts[0] || !$parts[1] || !$parts[2])
-            return false;
+            return [];
+
         return $parts;
     }
 
-    public static function validate(string $token) {
+    public static function validate(
+        string $token,
+        object $user
+    ): void {
+
+        if (!$token)
+            throw new \Exception('Missing token', 401);
+
         if (!$parts = static::validate_token_structure($token))
-            return false;
+            throw new \Exception('Malformed token', 401);
 
         $base64url_header = $parts[0];
-        $header = static::decode_base64url($base64url_header);
-        $base64url_payload = $parts[1];
-        $payload = static::decode_base64url($base64url_payload);
+        if (!$header = static::decode_base64url($base64url_header))
+            throw new \Exception('Malformed token: header', 401);
 
-        static::validate_token($payload, $header);
+        $base64url_payload = $parts[1];
+        if (!$payload = static::decode_base64url($base64url_payload))
+            throw new \Exception('Malformed token: payload', 401);
+
+        $time = strtotime('now');
+        static::validate_token($time, $payload, $header);
+
+        $user_id = $user->get_user_id();
+        $sub_user_id = (int)$payload['sub'];
+        if ($user_id != $sub_user_id)
+            throw new \Exception('Malformed token', 401);
 
         $table = new \util\table('User');
-        if (!$record = $table->select('JWTKey')->where([ 'UserID' => $payload['sub'] ])->query())
-            return false;
+        if (!$user = $table->select('JWTKey')->where(['UserID' => $user_id])->query())
+            throw new \Exception('Malformed token', 401);
 
+        $user_jwt_key = $user['JWTKey'];
+        $known_signature = static::generate_signature(static::HASH_ALG, $base64url_header, $base64url_payload, $user_jwt_key);
         $jwt_signature = $parts[2];
-        $known_signature = static::generate_signature(static::HASH_ALG, $base64url_header, $base64url_payload, $record['JWTKey']);
-        return hash_equals($known_signature, $jwt_signature);
+
+        if (!hash_equals($known_signature, $jwt_signature))
+            throw new \Exception('Malformed token', 401);
     }
 
-    private static function validate_token(array $payload, array $header) {
-        $time = strtotime('now');
+    private static function validate_token(
+        int $time,
+        array $payload,
+        array $header
+    ): void {
+
         if (empty($payload['exp']) || $time >= $payload['exp'])
             throw new \Exception('Token expired', 401);
+
         if (empty($header['alg']) || $header['alg'] != static::HASH_ALG)
             throw new \Exception('Malformed token', 401);
+
         if (empty($payload['sub']) || !\util\validate::id($payload['sub']))
             throw new \Exception('Malformed token', 401);
     }
 
-    private static function decode_base64url(string $base64url) {
+    private static function decode_base64url(string $base64url): array
+    {
         if (!$base64url)
-            return false;
+            return [];
+
         if (!$json_string = \util\base64::decode_url($base64url))
-            return false;
+            return [];
+
         return \util\json::decode($json_string);
     }
 
-    public static function create_key() {
+    public static function create_key(): string
+    {
         return \util\base64::encode(random_bytes(30));
     }
 }
